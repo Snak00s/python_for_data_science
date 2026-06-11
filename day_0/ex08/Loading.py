8d02485438d0c554e1772ebbf3b8cd65ea3eb8bd
Tom Pinton <tom.snak00s@gmail.com>
2026-06-11 14:27:30 +0200

    end day00

--- day_0/ex08/Loading.py
+++ day_0/ex08/Loading.py
@@ -1,17 +1,23 @@
 import os
 
+
 def ft_tqdm(lst: range) -> None:
-	for x in lst:
-		percent = int(((x + 1) / len(lst)) * 100)
-		less = os.get_terminal_size().columns - len(f"{percent}%|[]| {(x + 1)}/{len(lst)}") - 27
-		string = ""
-		less_threshold = int(less * percent / 100)
-		for y in range(less):
-			if y < less_threshold:
-				string += "="
-			elif y == less_threshold:
-				string += ">"
-			else:
-				string += " "
-		print(f"{percent}%|[{string}]| {(x + 1)}/{len(lst)}", end='\r')
-		yield
+    for x in lst:
+        percent = int(((x + 1) / len(lst)) * 100)
+        less = os.get_terminal_size().columns \
+            - len(f"{percent}%|[]| {(x + 1)}/{len(lst)}") - 27
+        string = ""
+        less_threshold = int(less * percent / 100)
+        for y in range(less):
+            if y < less_threshold:
+                string += "="
+            elif y == less_threshold:
+                string += ">"
+            else:
+                string += " "
+        if (less_threshold == less):
+            lststr = list(string)
+            lststr[-1] = ">"
+            string = "".join(lststr)
+        print(f"{percent}%|[{string}]| {(x + 1)}/{len(lst)}", end='\r')
+        yield
---
+++ day_0/ex09/ft_package/README.md
@@ -0,0 +1,1 @@
+Simple sample package from day00
